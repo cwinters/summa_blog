4bcb78f56c04539861800b325c4a8935b6e20fbb
Chris Winters <chris@cwinters.com>
2013-02-13 13:27:30 -0500

    add dependencies and other images

--- 201204-casper_intro/web/ajax_fetch.js
+++ 201204-casper_intro/web/ajax_fetch.js
@@ -4,6 +4,9 @@ $(document).ready( function() {
         $.get( '/partial.html' )
             .success( function( content ) {
                 $( '#target_html' ).empty().show().html( content );
+                setTimeout( function() {
+                    $( '#target_html' ).fadeOut();
+                }, 20000 );
             })
             .fail( function( error ) {
                 alert( "Failed to load partial: " + error );
@@ -13,6 +16,7 @@ $(document).ready( function() {
     // wire in the HTML button => short way
     var shortHtml = function() {
         $( '#target_html' ).empty().show().load( '/partial.html' );
+        setTimeout( function() { $( '#target_html' ).fadeOut() }, 20000 );
     };
 
     $( '#button_fetch_html' ).click( shortHtml );
@@ -23,6 +27,7 @@ $(document).ready( function() {
                 alert( "Got " + _.size( data.characters ) + " characters " );
                 var pretty = JSON.stringify( data, null, '    ' );
                 $( '#target_json' ).empty().show().html( pretty );
+                setTimeout( function() { $( '#target_json' ).fadeOut() }, 20000 );
             })
             .fail( function( error ) {
                 alert( "Failed to load JSON: " + error );
